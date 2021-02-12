c1f5813d43a64da79ead9c826ad09676bc450438
Bayan Bokan <beckbokanov@gmail.com>
2021-02-12 22:28:31 +0600

    todo app done

--- scripts/script.js
+++ scripts/script.js
@@ -5,41 +5,46 @@ const todoControl = document.querySelector('.todo-control'),
 	todoList = document.querySelector('.todo-list'),
 	todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = JSON.parse(localStorage.getItem('Данные о делах'));
+let todoData = JSON.parse(localStorage.getItem('Данные о делах'));
 
 const render = function () {
 	todoList.textContent = '';
 	todoCompleted.textContent = '';
 
-	todoData.forEach(function (item, i) {
-		const li = document.createElement('li');
-		li.classList.add('todo-item');
+	if (todoData !== null) {
+		todoData.forEach(function (item, i) {
+			const li = document.createElement('li');
+			li.classList.add('todo-item');
 
-		li.innerHTML = `<span class="text-todo">${item.value}</span>
-			<div class="todo-buttons">
-				<button class="todo-remove"></button>
-				<button class="todo-complete"></button>
-			</div>`;
+			li.innerHTML = `<span class="text-todo">${item.value}</span>
+				<div class="todo-buttons">
+					<button class="todo-remove"></button>
+					<button class="todo-complete"></button>
+				</div>`;
 
-		if (item.completed) {
-			todoCompleted.append(li);
-		} else {
-			todoList.append(li);
-		}
+			if (item.completed) {
+				todoCompleted.append(li);
+			} else {
+				todoList.append(li);
+			}
 
-		const btnTodoComplete = li.querySelector('.todo-complete');
-		btnTodoComplete.addEventListener('click', function () {
-			item.completed = !item.completed;
-			render();
+			const btnTodoComplete = li.querySelector('.todo-complete');
+			btnTodoComplete.addEventListener('click', function () {
+				item.completed = !item.completed;
+				render();
+			});
+
+			const btnTodoRemove = li.querySelector('.todo-remove');
+			btnTodoRemove.addEventListener('click', function () {
+				todoData.splice(i, 1);
+				render();
+			});
 		});
+	} else {
+		todoData = [];
+	}
 
-		const btnTodoRemove = li.querySelector('.todo-remove');
-		btnTodoRemove.addEventListener('click', function () {
-			todoData.splice(i, 1);
-			render();
-		});
 
-	});
 	let todoDataJSON = JSON.stringify(todoData);
 	localStorage.setItem('Данные о делах', todoDataJSON);
 };
